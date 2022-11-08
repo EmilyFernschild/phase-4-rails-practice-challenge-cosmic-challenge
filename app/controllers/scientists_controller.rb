e942bb8f2547bfe80af93b554ec92a8669d2f961
Emily Fernschild <emilyfernschild@gmail.com>
2022-11-08 15:12:32 -0500

    commit tree 8d5062af2c7f3738adda3d36f8596c55d5bb0179

---
+++ app/controllers/scientists_controller.rb
@@ -0,0 +1,35 @@
+class ScientistsController < ApplicationController
+
+    def index
+        render json: Scientist.all, status: :ok
+    end
+
+    def show 
+        scientist = Scientist.find(params[:id])
+        render json: scientist, status: :ok, serializer: ScientistPlanetSerializer
+    end
+
+    def create
+        scientist = Scientist.create!(sci_params)
+        render json: scientist, status: :created
+    end
+
+    def update 
+        scientist = Scientist.find(params[:id])
+        scientist.update!(sci_params)
+        render json: scientist, status: :accepted
+    end
+
+    def destroy
+        scientist = Scientist.find(params[:id])
+        scientist.destroy
+        head :no_content
+    end
+
+     private 
+
+     def sci_params
+        params.permit(:name, :field_of_study, :avatar)
+     end
+
+end
